304f0ba316b552cf21f62280e7d8cd344f16e997
oopsidoodles <vilimilner@gmail.com>
2018-10-08 21:45:45 -0400

    fixed some bat files

--- compileserver.bat
+++
@@ -1,8 +0,0 @@
-CD /D %~dp0
-CD tomcat
-CD webapps
-CD server
-CD WEB-INF
-CD classes
-
-javac -cp .;..\..\..\..\lib\servlet-api.jar MainServlet.java
--- startserver.bat
+++
@@ -1,4 +0,0 @@
-CD /D %~dp0
-cd tomcat
-CD bin
-startup.bat
